61ee25dc265048cab1c013c5e0fb2eca92bac1b8
vinay <vinayboi01@gmail.com>
2019-04-26 08:44:20 +0530

    Google map commit

--- src/app/pages/pagetwo/pagetwo.component.ts
+++ src/app/pages/pagetwo/pagetwo.component.ts
@@ -10,22 +10,32 @@ export class PagetwoComponent implements OnInit {
 
   latitude: number;
   longitude: number;
+  places: any = [];
 
   constructor(public googleMapService: GooglemapService) {
   }
 
   ngOnInit() {
     this.getCurrentPosition();
+    this.places = this.googleMapService.getPlaces();
+    console.log(this.places);
   }
 
   getCurrentPosition () {
     this.googleMapService.getPosition().subscribe((position: Position) => {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
-      console.log();
     }, error => {
       console.log('Unable to get position....', error);
     });
+  }
+
+  mapOnClickListener (event) {
+    this.places = this.googleMapService.addPlaceOnMapClick(event);
+  }
+
+  markerOnClickListener (place, index) {
+    alert('Lat and Lng::::' + JSON.stringify(place) + 'Index:::::' + index);
   }
 
 
--- src/app/services/googlemap/googlemap.service.ts
+++ src/app/services/googlemap/googlemap.service.ts
@@ -6,14 +6,30 @@ import { Observable } from 'rxjs';
 })
 export class GooglemapService {
 
+  currentPosition: any;
+  places: any = [];
+  place: any;
+
   constructor() { }
 
-  public getPosition(): Observable<Position> {
+  public getPosition () {
     return Observable.create((observer) => {
       navigator.geolocation.watchPosition((position: Position) => {
+        this.place = { lat: position.coords.latitude, lng: position.coords.longitude };
+        this.places.push(this.place);
         observer.next(position);
       });
     });
+  }
+
+  addPlaceOnMapClick (event) {
+    this.place = { lat: event.coords.lat, lng: event.coords.lng };
+    this.places.push(this.place);
+    return this.places;
+  }
+
+  getPlaces () {
+    return this.places;
   }
 
 }
